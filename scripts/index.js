ca95f2e1c7798c5dab20007e49c90e231fadbe03
Kebede <johnzeeze@yahoo.com>
2025-04-03 09:14:14 -0700

    Updated post abd edit modals

--- scripts/index.js
+++ scripts/index.js
@@ -30,45 +30,75 @@ const profileSection = document.querySelector(".profile");
 
 const profileEditButton = profileSection.querySelector(".profile__button-edit");
 
+const profilePostButton = profileSection.querySelector(".profile__button-post");
+
 const profileTitle = profileSection.querySelector(".profile__title");
 
 const profileDescription = profileSection.querySelector(
   ".profile__description"
 );
-// selecting required elements from modal section.
-const modalSection = document.querySelector("#modal-edit");
 
-const modalCloseButton = modalSection.querySelector(".modal__close-btn");
-
-const modalNameInput = modalSection.querySelector("#name");
-
-const modalDescriptionInput = modalSection.querySelector("#description");
-
-const modalSaveButton = modalSection.querySelector(".modal__submit-btn");
-
-// Adding event listener to the button selected elements.
-profileEditButton.addEventListener("click", function () {
-  modalSection.classList.add("modal_is-opened");
-  modalNameInput.value = profileTitle.textContent;
-  modalDescriptionInput.value = profileDescription.textContent;
-});
-
-modalCloseButton.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modalSection.classList.remove("modal_is-opened");
-});
-
-modalSaveButton.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  profileTitle.textContent = modalNameInput.value;
-  profileDescription.textContent = modalDescriptionInput.value;
-  modalNameInput.value = "";
-  modalDescriptionInput.value = "";
-  modalSection.classList.remove("modal_is-opened");
-});
-// cloning template content & selecting the <ul> element("".cards__list").
 const template = document.querySelector(".template").content;
 const cardsContainer = document.querySelector(".cards__list");
+// selecting required elements from modal section.
+
+const modalEdit = document.querySelector("#edit-profile-modal");
+const modalEditCloseButton = modalEdit.querySelector(".modal__close-btn");
+const modalFormEdit = modalEdit.querySelector(".modal__form");
+
+const modalEditNameInput = modalEdit.querySelector("#name");
+const modalEditDescriptionInput = modalEdit.querySelector("#description");
+
+const modalPost = document.querySelector("#new-post-modal");
+const modalPostCloseButton = modalPost.querySelector(".modal__close-btn");
+const modalFormPost = modalPost.querySelector(".modal__form");
+
+const modalPostCardImg = modalPost.querySelector("#card-img-input");
+const modalPostCardCaption = modalPost.querySelector("#card-img-caption");
+// Adding event listener to the button selected elements.
+profileEditButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalEdit.classList.add("modal_is-opened");
+  modalEditNameInput.value = profileTitle.textContent;
+  modalEditDescriptionInput.value = profileDescription.textContent;
+});
+
+modalEditCloseButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalEdit.classList.remove("modal_is-opened");
+});
+
+modalFormEdit.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  profileTitle.textContent = modalEditNameInput.value;
+  profileDescription.textContent = modalEditDescriptionInput.value;
+  modalEdit.classList.remove("modal_is-opened");
+});
+
+profilePostButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalPost.classList.add("modal_is-opened");
+});
+
+modalPostCloseButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalPost.classList.remove("modal_is-opened");
+});
+
+modalFormPost.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  const cardImg = modalPostCardImg.value;
+  const cardCaption = modalPostCardCaption.value;
+  const cardItem = {
+    name: cardCaption,
+    link: cardImg,
+  };
+  console.log(cardItem);
+  const cardElement = getCardElement(cardItem);
+  cardsContainer.append(cardElement);
+
+  modalPost.classList.remove("modal_is-opened");
+});
 
 function getCardElement(data) {
   const card = template.querySelector(".card").cloneNode(true);
